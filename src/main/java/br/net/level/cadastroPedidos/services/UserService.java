234f0510aefb0ea257c692eebbabd4bf3572e01c
Charles Oliveira <charlesoliveira@gmail.com>
2019-12-19 19:54:18 -0300

    Service Layer

---
+++ src/main/java/br/net/level/cadastroPedidos/services/UserService.java
@@ -0,0 +1,27 @@
+package br.net.level.cadastroPedidos.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.net.level.cadastroPedidos.entities.User;
+import br.net.level.cadastroPedidos.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
