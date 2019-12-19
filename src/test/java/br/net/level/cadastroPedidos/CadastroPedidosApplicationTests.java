337bcc439a0f9df44482feecf196c63469af2207
Charles Oliveira <charlesoliveira@gmail.com>
2019-12-19 18:37:49 -0300

    Projeto criado

---
+++ src/test/java/br/net/level/cadastroPedidos/CadastroPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.level.cadastroPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
